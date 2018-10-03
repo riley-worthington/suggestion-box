e2458a085cbd5d6ad3f109753b5ce10bad32570f
Riley Worthington <worthirj@whitman.edu>
2018-10-02 17:11:58 -0700

    add registration actions

--- frontend/src/Auth/authActions.js
+++ frontend/src/Auth/authActions.js
@@ -1,25 +1,26 @@
 import history from '../helpers/history';
 import { users } from '../fakeDatabase';
 import { SIGNIN_REQUEST, SIGNIN_SUCCESS, SIGNIN_FAILURE, SIGNOUT } from './authConstants';
+import { REGISTER_REQUEST, REGISTER_SUCCESS, REGISTER_FAILURE } from './authConstants';
 
 const sample_user = users[1];
 
-export const signIn = (username, password) => dispatch => {
+export const signIn = (email, password) => dispatch => {
   console.log('signing in');
-  console.log('username: ', username, 'password: ', password);
+  console.log('email: ', email, 'password: ', password);
 
   // Make API call here
-  dispatch(signInRequest(username, password))
+  dispatch(signInRequest(email, password))
 
   history.push('/');
   localStorage.setItem('user', JSON.stringify(sample_user));
   dispatch(signInSuccess(sample_user));
 
-  function signInRequest(username, password) {
+  function signInRequest(email, password) {
     return {
       type: SIGNIN_REQUEST,
       payload: {
-        username,
+        email,
         password
       }
     }
@@ -46,4 +47,36 @@ export const signOut = () => {
   return {
     type: SIGNOUT
   };
+}
+
+export const register = (firstName, lastName, email, password) => dispatch => {
+  dispatch(registerRequest(firstName, lastName, email, password));
+
+  // Send data to API, get user object as response
+
+  function registerRequest(firstName, lastName, email, password) {
+    return {
+      type: REGISTER_REQUEST,
+      payload: {
+        firstName,
+        lastName,
+        email,
+        password
+      }
+    }
+  }
+
+  function registerSuccess(user) {
+    return {
+      type: REGISTER_SUCCESS,
+      payload: user
+    }
+  }
+
+  function registerFailure(error) {
+    return {
+      type: REGISTER_FAILURE,
+      payload: error
+    }
+  }
 }
