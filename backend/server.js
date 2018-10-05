e5d9d9d178b5b9bc7ec5f61bf8331809655b424d
Riley Worthington <worthirj@whitman.edu>
2018-10-05 10:46:42 -0700

    time for backend

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.listen(3000, () => {
+  console.log('app is running on port 3000');
+});
